7728fda94dfc0e6427b4fc06c61ec694b10a6577
Igor Finagin <Igor@Finag.in>
2016-12-07 16:36:43 +0300

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
-(function ($) {
+(function ($, $$) {
+
+    if ($$.urlParams(debug)) {
+        $$.debug = true;
+    }
 
     $(function () {
 
@@ -62,6 +66,13 @@
             return item.length
         }
 
+        function firstImportant(m, d) {
+            if (+(new Date(sY, m, d)) > sT) {
+                return +(new Date(sY, m, d));
+            }
+            return +(new Date(sY + 1, m, d));
+        }
+
         var hD              = location.hash.replace("#", "").split("/").filter(notEmpty),
             sD              = hD[2] || 11,
             sM              = (hD[1] || 8) - 1,
@@ -76,6 +87,13 @@
                 new CoolDate(+(new Date(sY, sM + 3, sD)), "3 месяца", CoolDate.MONTH),
                 new CoolDate(+(new Date(sY, sM + 6, sD)), "Полгода", CoolDate.MONTH),
                 new CoolDate(+(new Date(sY, sM + 9, sD)), "9 месяцев", CoolDate.MONTH),
+                new CoolDate(+(new Date(sY + 1, sM, sD)), "Год", CoolDate.MONTH),
+                new CoolDate(+(new Date(sY + 1, sM + 6, sD)), "Полтора год", CoolDate.MONTH),
+                new CoolDate(+(new Date(sY + 2, sM, sD)), "2 года", CoolDate.MONTH),
+                new CoolDate(+(new Date(sY + 3, sM, sD)), "3 года", CoolDate.MONTH),
+                new CoolDate(+(new Date(sY + 4, sM, sD)), "4 года", CoolDate.MONTH),
+                new CoolDate(+(new Date(sY + 5, sM, sD)), "5 лет", CoolDate.MONTH),
+                new CoolDate(+(new Date(sY + 6, sM, sD)), "6 лет", CoolDate.MONTH),
 
                 /**
                  * Красивые числа
@@ -83,15 +101,20 @@
                 new CoolDate(sT + 111 * 864e5, "111 дней", CoolDate.COUNT),
                 new CoolDate(sT + 123 * 864e5, "123 дня", CoolDate.COUNT),
                 new CoolDate(sT + 200 * 864e5, "200 дней", CoolDate.COUNT),
-                new CoolDate(sT + 210 * 864e5, "2 1 0 дней", CoolDate.COUNT),
+                new CoolDate(sT + 210 * 864e5, "2 1 0", CoolDate.COUNT),
                 new CoolDate(sT + 222 * 864e5, "222 дня", CoolDate.COUNT),
+                new CoolDate(sT + 234 * 864e5, "234 дня", CoolDate.COUNT),
+                new CoolDate(sT + 300 * 864e5, "300 дней", CoolDate.COUNT),
+                new CoolDate(sT + 321 * 864e5, "3 2 1", CoolDate.COUNT),
+                new CoolDate(sT + 333 * 864e5, "333 дня", CoolDate.COUNT),
+                new CoolDate(sT + 345 * 864e5, "345 дней", CoolDate.COUNT),
 
                 /**
                  * Важные события
                  */
-                new CoolDate(+(new Date(sY + 1, 1, 14)), "Первое \"14 февраля\" вместе", CoolDate.IMPORTANT),
-                new CoolDate(+(new Date(sY + 1, 1, 23)), "Первое \"23 февраля\" вместе", CoolDate.IMPORTANT),
-                new CoolDate(+(new Date(sY + 1, 2, 8)), "Первое \"8 марта\" вместе", CoolDate.IMPORTANT)
+                new CoolDate(firstImportant(1, 14), "Первое \"14 февраля\" вместе", CoolDate.IMPORTANT),
+                new CoolDate(firstImportant(1, 23), "Первое \"23 февраля\" вместе", CoolDate.IMPORTANT),
+                new CoolDate(firstImportant(2, 8), "Первое \"8 марта\" вместе", CoolDate.IMPORTANT)
             ],
             nearest,
             nearestIterator = 0;
@@ -100,9 +123,11 @@
             return a.timestamp - b.timestamp;
         });
 
-        nearest = cool.filter(function (cool) {
-            return (cool.timestamp - tT) / 864e5 >= 0;
+        nearest = cool.filter(function (cool, index) {
+            return index < 10 && (cool.timestamp - tT) / 864e5 >= 0;
         });
+
+        $$.log(nearest);
 
         $(".title.days")
             .text(days);
@@ -137,7 +162,7 @@
 
     });
 
-})(jQuery);
+})(jQuery, Slavunya);
 
 (function () {
     var bar = new ProgressBar.Path('#heart-path', {
